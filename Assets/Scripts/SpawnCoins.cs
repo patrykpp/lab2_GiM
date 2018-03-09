43509c4c97d90bef4571e98a01f40c868dc61c53
patrykpp <patryk583@vp.pl>
2018-03-09 22:07:26 +0100

    lab2_GiM

---
+++ Assets/Scripts/SpawnCoins.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnCoins : MonoBehaviour {
+
+	public Transform[] coinSpawns;
+	public GameObject coin;
+    public GameObject coin2;
+
+    // Use this for initialization
+    void Start () {
+		Spawn (); // Generate some coins when the script is called.
+	}
+    // Spawn a random coin object
+
+    
+
+
+    void Spawn(){
+		// For however many coins we have, spawn them if a coin flips to 1.
+		for (int i =0; i < coinSpawns.Length; i++) {
+			int coinFlip = Random.Range(0,2); // Produce a value of 0 or 1
+			if(coinFlip > 0){
+                // coinSpawns[i] is our prefab.
+                if (i % 2 == 0) Instantiate(coin, coinSpawns[i].position, Quaternion.identity);
+                else Instantiate(coin2, coinSpawns[i].position, Quaternion.identity);
+            }
+		}
+	}
+}
---
+++ Assets/Scripts/pickupcoin.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class pickupcoin : MonoBehaviour {
+
+	// For now just destroy the coin if the player runs into
+	// it. Could play a sound, add to a score or more here later!
+	void OnTriggerEnter2D(Collider2D other){
+		if (other.gameObject.CompareTag ("Player")) {
+			Destroy(this.gameObject);
+			Debug.Log("Coin was picked up");
+		}
+	}
+}
